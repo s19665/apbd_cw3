f30338d5eb64767149c90b3fbd2a8b7fcfa29105
s19665 <s19665@pjwstk.edu.pl>
2020-03-18 20:41:04 +0100

    init and done

---
+++ FirstApi/FirstApi/DAL/IDbService.cs
@@ -0,0 +1,13 @@
+﻿using FirstApi.Modeles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApi.DAL
+{
+    public interface IDbService
+    {
+        public IEnumerable<Student> GetStudents();
+    }
+}
---
+++ FirstApi/FirstApi/DAL/MockDbService.cs
@@ -0,0 +1,26 @@
+﻿using FirstApi.Modeles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApi.DAL
+{
+    public class MockDbService : IDbService
+    {
+        private static IEnumerable<Student> _students;
+        static MockDbService()
+        {
+            _students = new List<Student>
+            {
+                new Student{IdStudent=1, FirstName="Jan", LastName="Kowalski"},
+                new Student{IdStudent=2, FirstName="Anna", LastName="Malewski"},
+                new Student{IdStudent=3, FirstName="Andrzej", LastName="Andrzejewicz"}
+            };
+        }
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
